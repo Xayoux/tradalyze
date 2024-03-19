55cb72ff6c953083256411c2e79c9eb608a21a87
Romain Capliez <romain.capliez01@gmail.com>
2024-03-19 10:14:22 +0100

    Ajout du df des codes pays utilisés dans BACI

---
+++ R/country_codes_V202401.R
@@ -0,0 +1,10 @@
+#' Codes pays utilisés dans la base BACI + correspondance avec codes iso3
+#'
+#' @format ## `country_codes_V202401`
+#' Un dataframe avec 238 lignes et 2 colonnes :
+#' \describe{
+#'   \item{country_code}{Codes pays utilisés dans la base BACI}
+#'   \item{country_iso3}{Correspondance avec les codes iso3}
+#' }
+#' @source <http://www.cepii.fr/DATA_DOWNLOAD/baci/data/BACI_HS92_V202401.zip>
+"country_codes_V202401"
---
+++ data-raw/country_codes_V202401.R
@@ -0,0 +1,5 @@
+## code to prepare `country_codes_V202401` dataset goes here
+country_codes_V202401 <-
+  read_csv("data-raw/country_codes_V202401.csv") |>
+  dplyr::select(country_code, country_iso3)
+usethis::use_data(country_codes_V202401, overwrite = TRUE)
