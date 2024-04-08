82c1d34514f4f06092854486c46af27a985a088e
Romain Capliez <romain.capliez01@gmail.com>
2024-04-08 16:44:43 +0200

    Ajout de la fonction eval_outliers

--- R/global_bindings.R
+++ R/global_bindings.R
@@ -3,4 +3,6 @@ utils::globalVariables(c("year", "code", "v", "k", "uv", "mean_diff", "i",
                          "extreme", "j", "v_share", "gamme_fontagne_1997",
                          "med_ref_ijkt", "v_t_k_i", "market_share_t_k_i",
                          "v_t_k_i_j", "market_share_t_k_i_j", "country_code",
-                         "country_iso3"))
+                         "country_iso3", "chapter", "total_v_without_outliers",
+                         "total_q_without_outliers", "total_v", "total_q",
+                         "ratio_v", "ratio_q", "quantile_H", "ratio_value"))
