1f9af5dd237a52432f09fdd68394381afb8569f3
Romain Capliez <romain.capliez01@gmail.com>
2024-05-21 10:11:50 +0200

    Création fonction adressed_demand

---
+++ R/adressed_demand.R
@@ -0,0 +1,190 @@
+#' @title
+#' Calculer la demande adressée à chaque pays
+#'
+#' @description
+#' Cacule la demande adressée à chaaue pays pour chaque année. La
+#' demande adressée peut être calculée par pays ou par groupement de produits.
+#' Le résultat retourné peut être la valeur de la demande adressée, la demande
+#' adressée en base 100 ou bien le ratio de la abse 100 avec la base 100 d'un
+#' pays de référence.
+#'
+#'
+#' @param baci Chemin d'accès, dataframe ou format parquet des données de baci
+#' à utiliser.
+#' @param years Années à garder dans les données.
+#' @param codes Codes à garder dans les données.
+#' @param year_ref Année de référence pour le calcul de la demande adressée.
+#' @param var_exporter Variable contenant les exportateurs.
+#' @param var_k Variable à utiliser pour le groupement des produits.
+#' @param exporter_ref Exportateur de référence pour le calcul du ratio de la
+#' demande adressée.
+#' @param base_100 Booléen indiquant si la demande adressée doit ête calculée
+#' en base 100 par rapport à l'année de référence.
+#' @param compare Booléen indiquant si le ratio de la demande adressée doit être
+#' calculé par rapport à un pays de référence.
+#' @param return_output Booléen indiquant si le résultat doit être retourné.
+#' @param return_pq Booléen indiquant si le résultat doit être retourné en format
+#' parquet.
+#' @param path_output Chemin d'accès pour enregistrer le résultat. Peut être
+#' en format csv ou parquet.
+#'
+#' @return Un dataframe contenant la demande adressée.
+#' @export
+#'
+#' @examples # Pas d'exemples.
+adressed_demand <- function(baci, years = NULL, codes = NULL, year_ref, var_exporter,
+                            var_k, exporter_ref = NULL, base_100 = TRUE,
+                            compare = FALSE,
+                            return_output = TRUE, return_pq = FALSE,
+                            path_output = NULL){
+
+  # Ouvrir les données de BACI
+  if (is.character(baci) == TRUE){
+    # Ouvrir les données depuis un dossier parquet (si chemin c'est parquet)
+    df_baci <-
+      baci |>
+      arrow::open_dataset()
+  }
+  else if (is.data.frame(baci) == TRUE){
+    # Ouvrir les données depuis un dataframe : passage en format arrow
+    df_baci <-
+      baci |>
+      arrow::arrow_table()
+  }
+  else{
+    # Ouvrir les données depuis format arrow : rien à faire
+    df_baci <- baci
+  }
+
+
+  # Garder les années voulues si years != NULL
+  if(!is.null(years)){
+    df_baci <-
+      df_baci |>
+      dplyr::filter(t %in% years)
+  }
+
+  # Garder les codes voulus si codes != NULL
+  if(!is.null(codes)){
+    df_baci <-
+      df_baci |>
+      dplyr::filter(k %in% codes)
+  }
+
+
+  # Calcul des poids de chaque marché pour tous les exportateurs
+  df_poids <-
+    baci |>
+    # Les poids ne sont calculé que par rapport à l'année de référence
+    dplyr::filter(
+      t == year_ref
+    ) |>
+    dplyr::collect() |>
+    # Calculer ce que le marché (pays-produit) représente dans les exportation du pays
+    # par catégroies de var_k
+    dplyr::mutate(
+      .by = c({{var_exporter}}, {{var_k}}, t),
+      poids = v / sum(v, na.rm = TRUE)
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::select(k, {{var_k}}, poids, {{var_exporter}}, importer) |>
+    # arrange(k, t, importer) |>
+    dplyr::arrange(k, dplyr::desc(poids))
+
+
+  # Calculer la demande adressée pour chaque pays, chaque année
+  df_da <-
+    baci |>
+    dplyr::select(-c(uv, med_ref_t_k, gamme_fontagne_1997, exporter_iso_region, importer_iso_region)) |>
+    dplyr::collect() |>
+    # Calcul di total des imports de chaque importateur pour chaque produit
+    dplyr::summarize(
+      .by = c({{var_k}}, t, importer, k),
+      total_import_k = sum(v, na.rm = TRUE)
+    ) |>
+    # Joindre les poids calculés précédemment un poids par importer-k
+    dplyr::left_join(
+      df_poids,
+      dplyr::join_by({{var_k}}, importer, k),
+      relationship = "many-to-many"
+    ) |>
+    # Si pas de poids = pas d'exports vers le pays = 0
+    dplyr::mutate(poids = tidyr::replace_na(poids, 0)) |>
+    # pour chaque exporter-t-produit : somme des poids * total_import_k
+    dplyr::summarize(
+      .by = c({{var_exporter}}, t, {{var_k}}),
+      DA = sum(poids * total_import_k, na.rm = TRUE)
+    ) |>
+    dplyr::filter(!is.na({{var_exporter}}))
+
+
+  if (base_100 == TRUE){
+    # Isoler la demande adressée de l'année de référence
+    # Sert à calculer la base 100
+    df_da_2010 <-
+      df_da |>
+      dplyr::filter(t == year_ref) |>
+      # Supprimer lan variable t, pour que le join aille sur toutes les lignes
+      dplyr::select(-t) |>
+      dplyr::rename(DA_2010 = DA)
+
+
+    # Caculer la demande adressée en base 100 par rapport à l'année de référence
+    df_da <-
+      df_da |>
+      dplyr::left_join(
+        df_da_2010,
+        dplyr::join_by({{var_exporter}}, {{var_k}})
+      )  |>
+      dplyr::mutate(
+        DA_100 = DA / DA_2010 * 100
+      )
+
+    if (compare == TRUE){
+      # Isoler la demande adressée de l'exportateur de référence
+      df_da_100_exporter_ref <-
+        df_da|>
+        dplyr::filter(!!dplyr::sym(var_exporter) == exporter_ref) |>
+        dplyr::select(t, {{var_k}}, DA_100) |>
+        dplyr::rename(DA_100_exporter_ref = DA_100)
+
+      # Calculer le ratio entre les et les DA du pays de ref
+      df_da <-
+        df_da |>
+        dplyr::filter(!!{{var_exporter}} != exporter_ref) |>
+        dplyr::left_join(
+          df_da_100_exporter_ref,
+          dplyr::join_by({{var_k}}, t)
+        ) |>
+        dplyr::mutate(
+          DA_diff = DA_100 / DA_100_exporter_ref
+        ) |>
+        dplyr::filter(!is.na(DA_diff))
+    }
+  }
+
+  # Retourner le résultat
+ if (return_output == TRUE){
+   if (return_pq == TRUE){
+     df_da <-
+       df_da |>
+       arrow::arrow_table()
+   }
+
+   return(df_da)
+ }
+
+  # Enregistrer le résultat
+  if (!is.null(path_output)){
+    if (tools::file_ext(path_output) == "csv"){
+      df_da |>
+        readr::write_csv(path_output)
+    }
+    else if (tools::file_ext(path_output) == "pq"){
+      df_da |>
+        arrow::write_parquet(path_output)
+    }
+  }
+
+
+}
