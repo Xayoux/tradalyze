fc65d52eabf544584ae4205ce06bc9a347ab407f
Romain CAPLIEZ <romain.capliez01@gmail.com>
2024-06-07 14:41:59 +0200

    Ajout possibilité de pondéré par des poids fixes sur une année

--- R/uv_comp.R
+++ R/uv_comp.R
@@ -1,3 +1,4 @@
+# Documentation -----------------------------------------------------------
 #' @title
 #' Comparaison des valeurs unitaires
 #'
@@ -42,18 +43,34 @@
 #' moyenne pondérée, ou "median_pond" pour la médiane pondérée.
 #' @param var_pond Variable à utiliser pour la pondération des valeurs unitaire
 #' lorsque formula demande une pondération.
+#' @param fixed_weight Booléen indiquant si les poids utilisés pour la
+#' pondération doivent être fixés à une année ou non.
+#' @param year_ref_fixed_weight Année à partir dequels les poids fixes doivent
+#' être calculés si nécessaire.
+#' @param var_desagregate Vecteur de variables (sous forme de chaînes de
+#' caractères) indiquant le niveau le plus désagrégé possible des données
+#' fournies pour l'année, les exportateurs, importateurs et les produits.
+#' Ces variables servent à définir les poids fixes pour chaque flux. Par
+#' défaut les variables sont `c("t", "exporter", "importer", "k")`. Il n'est
+#' normalement pas nécessaire de les changer si les données utilisées
+#' proviennent des fonctions de ce package.
 #'
 #' @return Un dataframe contenant l'évolution des valeurs unitaires.
 #' @export
 #'
 #' @examples # Pas d'exemples.
+# Fonction uv_comp ---------------------------------------------------------
+## Définition de la fonction -----------------------------------------------
 uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
-                    var_pond = NULL, year_ref, var_exporter,
-                            var_k, exporter_ref = NULL, base_100 = TRUE,
-                            compare = FALSE,
-                            return_output = TRUE, return_pq = FALSE,
-                            path_output = NULL){
+                    var_pond = NULL, fixed_weight = FALSE,
+                    year_ref, var_exporter, year_ref_fixed_weight = year_ref,
+                     var_desagregate = c("t", "exporter", "importer", "k"),
+                    var_k, exporter_ref = NULL, base_100 = TRUE,
+                    compare = FALSE,
+                    return_output = TRUE, return_pq = FALSE,
+                    path_output = NULL){
 
+  ## Importation des données ----------------------------------------------
   # Ouvrir les données de BACI
   if (is.character(baci) == TRUE){
     # Ouvrir les données depuis un dossier parquet (si chemin c'est parquet)
@@ -86,6 +103,8 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
       dplyr::filter(k %in% codes)
   }
 
+
+  ## Calcul des valeurs unitaires -----------------------------------------
   # Faire la moyenne des valeurs unitaires pour chaque année, exporter, produits
   if(formula == "mean"){
     df_uv <-
@@ -95,7 +114,9 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
         uv = mean(uv, na.rm = TRUE)
       )
   }
-  else if (formula == "median"){
+  
+  # Faire la médiane
+  if (formula == "median"){
     df_uv <-
       df_baci |>
       dplyr::summarize(
@@ -103,7 +124,33 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
         uv = stats::median(uv, na.rm = TRUE)
       )
   }
-  else if (formula == "median_pond"){
+
+
+  # Uniquement sur les calculs avec des pondérations
+  # Si les poids sont fixes, calculés les poids de l'année de référence
+  # Les associer aux flux
+  if (fixed_weight == TRUE){
+    df_pond <-
+      df_baci |>
+      dplyr::filter(t == year_ref_fixed_weight) |>
+      dplyr::select({{var_desagregate}}, {{var_pond}}) |>
+      dplyr::select(-t)
+
+    var_desagregate_join <-
+      var_desagregate[var_desagregate != "t"]
+
+    df_baci <-
+      df_baci |>
+      dplyr::select(-{{var_pond}}) |>
+      dplyr::left_join(
+        df_pond,
+        by = var_desagregate_join
+      )
+  }
+  
+  
+  # Faire la médiane pondérée
+  if (formula == "median_pond"){
     df_uv <-
       df_baci |>
       dplyr::collect() |>
@@ -112,7 +159,9 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
         uv = matrixStats::weightedMedian(uv, w = !!dplyr::sym(var_pond), na.rm = TRUE)
       )
   }
-  else if (formula == "mean_pond"){
+  
+  # Faire la moyenne pondérée
+  if (formula == "mean_pond"){
     df_uv <-
       df_baci |>
       dplyr::collect() |>
@@ -123,7 +172,7 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
   }
 
 
-
+  ## Base 100 -------------------------------------------------------------
   # Mettre en base 100 si voulu
   if (base_100 == TRUE){
     df_uv_year_ref <-
@@ -143,6 +192,8 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
       ) |>
       dplyr::select(-c(uv_year_ref, uv))
 
+
+  ## Comparaison avec un pays ---------- ----------------------------------
     if (compare == TRUE){
       df_uv_exporter_ref <-
         df_uv |>
@@ -164,6 +215,8 @@ uv_comp <- function(baci, years = NULL, codes = NULL, formula = "median_pond",
     }
   }
 
+
+  ## Exportation des résultats --------------------------------------------
   # Retourner le résultat
   if (return_output == TRUE){
     if (return_pq == TRUE){
