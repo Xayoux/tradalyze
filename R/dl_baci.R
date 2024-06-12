d8c4bfa54a499edf667f1572b29d74c30405ead0
Romain CAPLIEZ <romain.capliez01@gmail.com>
2024-06-12 16:10:54 +0200

    Boiter de dialogue au lieu de message dans la console

--- R/dl_baci.R
+++ R/dl_baci.R
@@ -79,61 +79,11 @@ dl_baci <- function(revision = "HS92", dl_folder, rm_csv = TRUE,
     rvest::html_elements("em") |>
     rvest::html_text()
 
-  # Demander si l'utilisateur veut télécharger cette version de baci : version interactove seulement
-  if (interactive()){
-    question_dl <- stringr::str_glue("Voulez-vous t\uE9l\uE9charger BACI {version} ? (Y/n) : ")
-    rep_dl_baci <- readline(prompt = question_dl)
+  # Demander si l'utilisateur veut télécharger cette version de baci
+  question_dl <- stringr::str_glue("Voulez-vous t\u00E9l\u00E9charger BACI {version} ?")
+  reponse_dl <- svDialogs::dlg_message(question_dl, "yesnocancel")$res
 
-    if (toupper(rep_dl_baci) == "Y"){
-      # Créer le lien pour télécharger la dernière version de BACI
-      dl_link <- stringr::str_glue("http://www.cepii.fr/DATA_DOWNLOAD/baci/data/BACI_{revision}_V{version}.zip")
-
-      # # Tester si le fichier zip de BACI existe déjà
-
-      # Si dl_zip == TRUE, télécharger le fichier zip
-      # Télécharge le zip même s'il existe
-      if (dl_zip == TRUE) {
-        curl::multi_download(
-          dl_link,
-          here::here(dl_folder, stringr::str_glue("BACI_{revision}_V{version}.zip"))
-        )
-      }
-      # Si dl_zip == FALSE, vérifier si le fichier zip existe.
-      # S'il existe alors, on ne télécharge pas. Sinon, on télécharge.
-      else {
-        if (!file.exists(here::here(dl_folder, stringr::str_glue("BACI_{revision}_V{version}.zip")))) {
-          # Si le fichier zip n'existe pas, télécharger BACI
-          curl::multi_download(
-            dl_link,
-            here::here(dl_folder, stringr::str_glue("BACI_{revision}_V{version}.zip"))
-          )
-        }
-      }
-
-      # Décompresser le fichier zip au même endroit
-      print("Extraction des fichier csv")
-      here::here(dl_folder, stringr::str_glue("BACI_{revision}_V{version}.zip")) |>
-        utils::unzip(exdir = dl_folder)
-
-      # Créer les formats parquet pour BACI
-      print("Cr\uE9ation des fichiers parquet")
-      analyse.competitivite::transfo_baci_pq(
-        csv_folder = dl_folder,
-        path_output = dl_folder,
-        version = version
-      )
-
-      # Supprimer les fichiers csv de BACI pour gain de place si rm_csv == TRUE
-      if (rm_csv == TRUE) {
-        dl_folder |>
-          list.files(full.names = TRUE, pattern = "^BACI.*csv") |>
-          purrr::walk(file.remove)
-      }
-      print("Donn\uE9es de BACI t\uE9l\uE9charg\uE9es !")
-    } else {
-      print(stringr::str_glue("Refus de t\uE9l\uE9charger BACI {version}"))
-    }
-  } else {
+  if (reponse_dl == "yes"){
     # Créer le lien pour télécharger la dernière version de BACI
     dl_link <- stringr::str_glue("http://www.cepii.fr/DATA_DOWNLOAD/baci/data/BACI_{revision}_V{version}.zip")
 
@@ -178,8 +128,10 @@ dl_baci <- function(revision = "HS92", dl_folder, rm_csv = TRUE,
         list.files(full.names = TRUE, pattern = "^BACI.*csv") |>
         purrr::walk(file.remove)
     }
+    print("Donn\u00E9es de BACI t\u00E9l\uE900charg\u00E9es !")
+  } else {
+    print(stringr::str_glue("Refus de t\u00E9l\u00E9charger BACI {version}"))
   }
-  
 }
 
 
--- R/dl_gravity.R
+++ R/dl_gravity.R
@@ -39,77 +39,13 @@ dl_gravity <- function(dl_folder, dl_zip = FALSE){
     rvest::html_text() |>
     stringr::str_extract("\\d{6}")
 
-  if (interactive()){
-    # Demander à l'utilisateur s'il souhaite dl cette version de gravity
-    question <- stringr::str_glue("Voulez-vous t\uE9l\uE9charger Gravity {version} ? (Y/n) : ")
-    reponse <- readline(prompt = question)
+  
+  # Demander à l'utilisateur s'il souhaite dl cette version de gravity
+  question_dl <- stringr::str_glue("Voulez-vous t\u00E9l\u00E9charger Gravity {version} ? (Y/n) : ")
+  reponse_dl <- svDialogs::dlg_message(question_dl, "yesnocancel")$res
 
-    if (toupper(reponse) == "Y"){
-      # Récupérer le lien de téléchargement du fichier zip de Gravity
-      dl_link <-
-        html_gravity |>
-        rvest::html_nodes("a") |>
-        rvest::html_attr("href") |>
-        (\(links) grep("csv", links, value = TRUE, ignore.case = TRUE))() |>
-                                                                      (\(link) grep(version, link, value = TRUE))()
-
-
-      # Chemin d'accès au dossier contenant la dernière version de Gravity
-      gravity_folder <-
-        here::here(
-          dl_folder,
-          stringr::str_glue("Gravity_csv_V{version}")
-        )
-
-      # Chemin d'accès au fichier zip de Gravity dans sa dernière version
-      gravity_zip <-
-        here::here(
-          gravity_folder,
-          stringr::str_glue("Gravity_csv_V{version}.zip")
-        )
-
-
-      # Créer le dossier où sera mis le zip téléchargé, s'il n'existe pas déjà
-      if (!dir.exists(here::here(dl_folder))) {
-        dir.create(gravity_folder, recursive = TRUE)
-      }
-
-
-      # Télécharger le fichier zip de Gravity
-      if (dl_zip == TRUE) { # Si TRUE télécharger dans tous les cas
-        curl::multi_download(
-          dl_link,
-          gravity_zip
-        )
-      }
-      else { # Si Faux télécharger le zip que s'il n'existe pas
-        if (!file.exists(gravity_zip)) {
-          # Si le fichier zip n'existe pas, télécharger BACI
-          curl::multi_download(
-            dl_link,
-            gravity_zip
-          )
-        }
-      }
-
-      # Décompresser le fichier zip au même endroit
-      print("Extraction des fichiers de Gravity")
-      gravity_zip |>
-        utils::unzip(exdir = gravity_folder)
-
-      # Créer les formats parquet de Gravity
-      print("Cr\uE9ation des fichiers parquet")
-      analyse.competitivite::transfo_gravity_pq(
-        csv_folder = gravity_folder,
-        path_output = gravity_folder,
-        version = version
-      )
-
-      print("Donn\uE9es de Gravity t\uE9l\uE9charg\uE9es")
-    } else {
-      print(stringr::str_glue("Refus de t\uE9l\uE9charger Gravity {version}"))
-    } 
-  } else {
+  if (reponse_dl == "yes"){
+    # Récupérer le lien de téléchargement du fichier zip de Gravity
     dl_link <-
       html_gravity |>
       rvest::html_nodes("a") |>
@@ -169,10 +105,12 @@ dl_gravity <- function(dl_folder, dl_zip = FALSE){
       version = version
     )
 
-    print("Donn\uE9es de Gravity t\uE9l\uE9charg\uE9es")
-  }
-  
-}
+    print("Donn\u00E9es de Gravity t\u00E9l\u00E9charg\u00E9es")
+  } else {
+    print(stringr::str_glue("Refus de t\u00E9l\u00E9charger Gravity {version}"))
+  } 
+} 
+
 
 
 
