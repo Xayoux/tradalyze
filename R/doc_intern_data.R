70e8c186cdad293cc8c1d75c0e24f92ae6e81b97
Romain CAPLIEZ <romain.capliez01@gmail.com>
2024-08-05 11:29:24 +0200

    Translate documentation

--- R/doc_intern_data.R
+++ R/doc_intern_data.R
@@ -1,14 +1,15 @@
 # chelem_classification ---------------------------------------------------
-#' Classification géographique utilisée par la base CHELEM au niveau de grandes régions
+#' Geographic classification used by the database
+#' [CHELEM] (<http://www.cepii.fr/CEPII/fr/bdd_modele/bdd_modele_item.asp?id=17>).
 #'
 #' @format ## `classification_chelem`
-#' Un dataframe avec 196 lignes et 3 colonnes :
+#' A dataframe with 196 rows and 3 columns :
 #' \describe{
-#'   \item{iso_region}{Code iso3 utilisé pour définir les régions géographiques.}
-#'   \item{name_region}{Le nom associé à la région.}
-#'   \item{iso_country}{Code iso3 du pays entrant dans la composition de la région}
+#'   \item{iso_region}{Str : Character iso code 3 use for the region.}
+#'   \item{name_region}{Str : The name of the region.}
+#'   \item{iso_country}{Str : Character iso code 3 use for the country}
 #' }
-#' @source [Classification Chelem du CEPII](<http://www.cepii.fr/CEPII/fr/bdd_modele/bdd_modele_item.asp?id=17>). Voir :
+#' @source [CHELEM classificiation](<http://www.cepii.fr/CEPII/fr/bdd_modele/bdd_modele_item.asp?id=17>). See :
 #' [de Saint Vaulry, A. (2008), “Base de données CHELEM - Commerce international du CEPII”,  Document de travail du CEPII, N°2008-09](http://www.cepii.fr/cepii/fr/publications/wp/abstract.asp?nodoc=1081)
 "chelem_classification"
 
